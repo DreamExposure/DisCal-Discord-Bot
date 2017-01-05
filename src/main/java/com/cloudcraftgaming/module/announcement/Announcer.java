054f220ccfd1c810696d73054fed59926d741da9
NovaFox161 <novaschweda@gmail.com>
2017-01-05 16:35:45 -0600

    Start basic work on Announcement system.

---
+++ src/main/java/com/cloudcraftgaming/module/announcement/Announcer.java
@@ -0,0 +1,9 @@
+package com.cloudcraftgaming.module.announcement;
+
+/**
+ * Created by Nova Fox on 1/5/2017.
+ * Website: www.cloudcraftgaming.com
+ * For Project: DisCal
+ */
+public class Announcer {
+}
