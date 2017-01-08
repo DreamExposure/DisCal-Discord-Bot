3c4faaf238c724f7f2e49431c5df226925cb8117
NovaFox161 <novaschweda@gmail.com>
2017-01-07 21:34:39 -0600

    Add some stuff to the Announcer Object to manage loaded Announcements.

--- src/main/java/com/cloudcraftgaming/module/announcement/Announcer.java
+++ src/main/java/com/cloudcraftgaming/module/announcement/Announcer.java
@@ -1,5 +1,7 @@
 package com.cloudcraftgaming.module.announcement;
 
+import java.util.ArrayList;
+
 /**
  * Created by Nova Fox on 1/5/2017.
  * Website: www.cloudcraftgaming.com
@@ -8,6 +10,8 @@ package com.cloudcraftgaming.module.announcement;
 public class Announcer {
     private static Announcer instance;
 
+    private final ArrayList<Announcement> announcements = new ArrayList<>();
+
     private Announcer() {}
 
     public static Announcer getAnnouncer() {
@@ -15,5 +19,25 @@ public class Announcer {
             instance = new Announcer();
         }
         return instance;
+    }
+
+    public void init() {
+        //This method will load all existing announcements from the database once supported.
+    }
+
+    //Getters
+    public ArrayList<Announcement> getAnnouncements(String guildId) {
+        ArrayList<Announcement> _announcements = new ArrayList<>();
+        for (Announcement a : announcements) {
+            if (a.getGuildId().equals(guildId)) {
+                _announcements.add(a);
+            }
+        }
+        return _announcements;
+    }
+
+    //Setters
+    public void addAnnouncement(Announcement announcement) {
+        announcements.add(announcement);
     }
 }
