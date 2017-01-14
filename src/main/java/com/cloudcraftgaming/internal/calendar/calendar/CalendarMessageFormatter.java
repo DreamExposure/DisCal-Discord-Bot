243503fe945f305e53451afb5783c19039417fc3
NovaFox161 <novaschweda@gmail.com>
2017-01-13 20:32:15 -0600

    Add proper link calendar formatting. Now it looks pretty!

--- src/main/java/com/cloudcraftgaming/internal/calendar/calendar/CalendarMessageFormatter.java
+++ src/main/java/com/cloudcraftgaming/internal/calendar/calendar/CalendarMessageFormatter.java
@@ -1,6 +1,10 @@
 package com.cloudcraftgaming.internal.calendar.calendar;
 
+import com.cloudcraftgaming.database.DatabaseManager;
 import com.google.api.services.calendar.model.Calendar;
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+
+import java.net.URI;
 
 /**
  * Created by Nova Fox on 1/4/2017.
@@ -10,12 +14,16 @@ import com.google.api.services.calendar.model.Calendar;
 public class CalendarMessageFormatter {
     private static String lineBreak = System.getProperty("line.separator");
 
-    public static String getFormatEventMessage(Calendar calendar) {
+    public static String getFormatEventMessage(MessageReceivedEvent event, Calendar calendar) {
+        String calId = DatabaseManager.getManager().getData(event.getMessage().getGuild().getID()).getCalendarAddress();
+        URI callURI = URI.create(calId);
+        String link = "https://calendar.google.com/calendar/embed?src=" + callURI;
         return "~-~-~- Calendar Info ~-~-~-" + lineBreak
                 + "Calendar ID: " + calendar.getId() + lineBreak + lineBreak
                 + "Name/Summery: " + calendar.getSummary() + lineBreak + lineBreak
                 + "Description: " + calendar.getDescription() + lineBreak + lineBreak
-                + "TimeZone: " + calendar.getTimeZone();
+                + "TimeZone: " + calendar.getTimeZone() + lineBreak + lineBreak
+                + "Link: " + link;
     }
 
     public static String getFormatEventMessage(PreCalendar calendar) {
@@ -23,6 +31,7 @@ public class CalendarMessageFormatter {
                 + "Calendar ID: null until creation completed" + lineBreak + lineBreak
                 + "Name/Summery: " + calendar.getSummery() + lineBreak + lineBreak
                 + "Description: " + calendar.getDescription() + lineBreak + lineBreak
-                + "TimeZone: " + calendar.getTimezone();
+                + "TimeZone: " + calendar.getTimezone() + lineBreak + lineBreak
+                + "Link: Unknown until confirmed.";
     }
 }
--- src/main/java/com/cloudcraftgaming/module/command/LinkCalendarCommand.java
+++ src/main/java/com/cloudcraftgaming/module/command/LinkCalendarCommand.java
@@ -1,11 +1,14 @@
 package com.cloudcraftgaming.module.command;
 
 import com.cloudcraftgaming.database.DatabaseManager;
+import com.cloudcraftgaming.internal.calendar.CalendarAuth;
+import com.cloudcraftgaming.internal.calendar.calendar.CalendarMessageFormatter;
 import com.cloudcraftgaming.utils.Message;
+import com.google.api.services.calendar.model.Calendar;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 
-import java.net.URI;
+import java.io.IOException;
 
 /**
  * Created by Nova Fox on 1/3/2017.
@@ -20,10 +23,13 @@ public class LinkCalendarCommand implements ICommand {
 
     @Override
     public Boolean issueCommand(String[] args, MessageReceivedEvent event, IDiscordClient client) {
-        String calId = DatabaseManager.getManager().getData(event.getMessage().getGuild().getID()).getCalendarAddress();
-        URI callURI = URI.create(calId);
-        String link = "https://calendar.google.com/calendar/embed?src=" + callURI;
-        Message.sendMessage("Calendar: " + link, event, client);
+        try {
+            String calId = DatabaseManager.getManager().getData(event.getMessage().getGuild().getID()).getCalendarAddress();
+            Calendar cal = CalendarAuth.getCalendarService().calendars().get(calId).execute();
+            Message.sendMessage(CalendarMessageFormatter.getFormatEventMessage(event, cal), event, client);
+        } catch (IOException e) {
+            Message.sendMessage("Oops! Something went wrong! Please report this to the developer!", event, client);
+        }
         return false;
     }
 }
