bd096fec2e9e86c0368fd7a1b141c43ecdd2a97f
Eloaen <shadowjacob@yahoo.com>
2017-03-01 18:27:16 -0600

    Added a method to support Embeds
    Going along with my cleaning up of Messages

--- src/main/java/com/cloudcraftgaming/utils/Message.java
+++ src/main/java/com/cloudcraftgaming/utils/Message.java
@@ -3,6 +3,8 @@ package com.cloudcraftgaming.utils;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 import sx.blah.discord.util.MessageBuilder;
+import sx.blah.discord.api.internal.json.objects.EmbedObject;
+
 
 /**
  * Created by Nova Fox on 1/3/2017.
@@ -15,6 +17,14 @@ public class Message {
     public static void sendMessage(String message, MessageReceivedEvent event, IDiscordClient client) {
         try {
             new MessageBuilder(client).appendContent(message).withChannel(event.getMessage().getChannel()).build();
+        } catch (Exception e) {
+            //Failed to send message.
+        }
+    }
+    
+    public static void sendMessage(EmbedObject embed, MessageReceivedEvent event, IDiscordClient client) {
+        try {
+            new MessageBuilder(client).withEmbed(embed).withChannel(event.getMessage().getChannel()).build();
         } catch (Exception e) {
             //Failed to send message.
         }
