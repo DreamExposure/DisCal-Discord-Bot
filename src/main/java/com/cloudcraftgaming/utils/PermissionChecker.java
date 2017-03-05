30926a1fff270ff11cffa4a2c3302ca1490bf506
NovaFox161 <novaschweda@gmail.com>
2017-03-05 14:23:25 -0600

    Temporarily catch errors with perms checker to avoid some reported issues.

--- src/main/java/com/cloudcraftgaming/utils/PermissionChecker.java
+++ src/main/java/com/cloudcraftgaming/utils/PermissionChecker.java
@@ -2,6 +2,7 @@ package com.cloudcraftgaming.utils;
 
 import com.cloudcraftgaming.database.DatabaseManager;
 import com.cloudcraftgaming.internal.data.BotData;
+import com.cloudcraftgaming.internal.email.EmailSender;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IRole;
 import sx.blah.discord.handle.obj.IUser;
@@ -13,17 +14,24 @@ import sx.blah.discord.handle.obj.IUser;
  */
 public class PermissionChecker {
     public static boolean hasSufficientRole(MessageReceivedEvent event) {
-        BotData bd = DatabaseManager.getManager().getData(event.getMessage().getGuild().getID());
-        if (!bd.getControlRole().equalsIgnoreCase("everyone")) {
-            IUser sender = event.getMessage().getAuthor();
-            String roleId = bd.getControlRole();
-            IRole role = event.getMessage().getGuild().getRoleByID(roleId);
+        //TODO: Figure out exactly what is causing a NPE here...
+        try {
+            BotData bd = DatabaseManager.getManager().getData(event.getMessage().getGuild().getID());
+            if (!bd.getControlRole().equalsIgnoreCase("everyone")) {
+                IUser sender = event.getMessage().getAuthor();
+                String roleId = bd.getControlRole();
+                IRole role = event.getMessage().getGuild().getRoleByID(roleId);
 
-            if (role != null) {
-                if (!event.getMessage().getGuild().getUsersByRole(role).contains(sender)) {
-                    return false;
+                if (role != null) {
+                    if (!event.getMessage().getGuild().getUsersByRole(role).contains(sender)) {
+                        return false;
+                    }
                 }
             }
+        } catch (Exception e) {
+            //Something broke so we will harmlessly allow access and email the dev.
+            EmailSender.getSender().sendExceptionEmail(e);
+            return true;
         }
         return true;
     }
