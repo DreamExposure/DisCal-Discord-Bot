974689634ad25b51860167b969aa6d259c192250
NovaFox161 <novaschweda@gmail.com>
2017-03-05 19:52:29 -0600

    Update version numbers

--- src/main/java/com/cloudcraftgaming/Main.java
+++ src/main/java/com/cloudcraftgaming/Main.java
@@ -22,7 +22,7 @@ import sx.blah.discord.util.DiscordException;
  */
 @SuppressWarnings("SameParameterValue")
 public class Main {
-    public static String version = "Beta 1.2.0";
+    public static String version = "Beta 2.0.0";
     public static IDiscordClient client;
 
     public static void main(String[] args) {
