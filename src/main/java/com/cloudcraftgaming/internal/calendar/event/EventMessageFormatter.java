de781425938dfd247c38b60963f861f5fc742937
NovaFox161 <novaschweda@gmail.com>
2017-03-05 19:20:49 -0600

    Forgot to add the conversion in this method, oops!

--- src/main/java/com/cloudcraftgaming/internal/calendar/event/EventMessageFormatter.java
+++ src/main/java/com/cloudcraftgaming/internal/calendar/event/EventMessageFormatter.java
@@ -72,16 +72,46 @@ public class EventMessageFormatter {
     private static String getHumanReadableTime(Event event, boolean start) {
         if (start) {
             String[] timeArray = event.getStart().getDateTime().toStringRfc3339().split(":");
+            String suffix = "";
             String hour = timeArray[0].substring(11, 13);
+
+            //Convert hour from 24 to 12...
+            try {
+                Integer hRaw = Integer.valueOf(hour);
+                if (hRaw > 12) {
+                    hour = String.valueOf(hRaw - 12);
+                    suffix = "PM";
+                } else {
+                    suffix = "AM";
+                }
+            } catch (NumberFormatException e) {
+                //I Dunno... just should catch the error now and not crash anything...
+            }
+
             String minute = timeArray[1];
 
-            return hour + ":" + minute;
+            return hour + ":" + minute + suffix;
         } else {
             String[] timeArray = event.getEnd().getDateTime().toStringRfc3339().split(":");
+            String suffix = "";
             String hour = timeArray[0].substring(11, 13);
+
+            //Convert hour from 24 to 12...
+            try {
+                Integer hRaw = Integer.valueOf(hour);
+                if (hRaw > 12) {
+                    hour = String.valueOf(hRaw - 12);
+                    suffix = "PM";
+                } else {
+                    suffix = "AM";
+                }
+            } catch (NumberFormatException e) {
+                //I Dunno... just should catch the error now and not crash anything...
+            }
+
             String minute = timeArray[1];
 
-            return hour + ":" + minute;
+            return hour + ":" + minute + suffix;
         }
     }
 
