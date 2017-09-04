e1ca3d6c05b517076ed13a89c77cfb2cae7eb35f
NovaFox161 <novaschweda@gmail.com>
2017-09-04 15:40:10 -0500

    Fix bad timezone checking (no longer set off by "UTC", etc)

--- src/main/java/com/cloudcraftgaming/discal/utils/TimeZoneUtils.java
+++ src/main/java/com/cloudcraftgaming/discal/utils/TimeZoneUtils.java
@@ -1,5 +1,6 @@
 package com.cloudcraftgaming.discal.utils;
 
+import com.cloudcraftgaming.discal.internal.calendar.timezone.BadTimezone;
 import org.joda.time.DateTimeZone;
 
 /**
@@ -8,8 +9,6 @@ import org.joda.time.DateTimeZone;
  * For Project: DisCal
  */
 public class TimeZoneUtils {
-	private static final String BAD_TZs = "America/Adak,America/Atka,US/Aleutian,HST,America/Juneau,America/Nome,America/Yakutat,America/Dawson,PST8PDT,America/Boise,America/Cambridge_Bay,America/Chihuahua,America/Inuvik,America/Ojinaga,MST,MST7MDT,America/Bahia_Banderas,America/Indiana/Knox,America/Indiana/Tell_City,America/Matamoros,America/Menominee,America/Merida,America/Monterrey,America/North_Dakota/Center,America/North_Dakota/New_Salem,America/Rainy_River,America/Rankin_Inlet,America/Swift_Current,America/Atikokan,America/Coral_Harbour,US/Michigan,America/Indiana/Indianapolis,America/Indiana/Marengo,America/Indiana/Petersburg,America/Indiana/Vevay,America/Indiana/Vincennes,America/Indiana/Winamac,America/Kentucky/Louisville,America/Louisville,America/Kentucky/Monticello,America/Nipigon,America/Pangnirtung,America/Resolute,America/Thunder_Bay,EST,EST5EDT,America/Argentina/San_Luis,America/Eirunepe,America/Glace_Bay,America/Goose_Bay,America/Moncton,America/Argentina/Catamarca,America/Argentina/Catamarca,America/Argentina/ComodRivadavia,America/Catamarca,America/Argentina/Cordoba,America/Cordoba,America/Rosario,America/Argentina/Jujuy,America/Jujuy,America/Argentina/La_Rioja,America/Argentina/Mendoza,America/Mendoza,America/Argentina/Rio_Gallegos,America/Argentina/Salta,America/Argentina/San_Juan,America/Argentina/Tucuman,America/Argentina/Ushuaia,America/Santarem,Atlantic/Madeira,Etc/UCT,UCT,Etc/UTC,Etc/Universal,Etc/Zulu,Universal,Zulu,CET,MET,EET,Europe/Simferopol,Europe/Uzhgorod,Europe/Zaporozhye,Europe/Volgograd,Asia/Oral,Asia/Samarkand,Asia/Novokuznetsk,Asia/Pontianak,Asia/Kashgar,Asia/Kuching,Asia/Urumqi,Asia/Sakhalin,Antarctica/Macquarie,Asia/Anadyr,Pacific/Chatham";
-
     public static boolean isValid(String value) {
         try {
             DateTimeZone tz = DateTimeZone.forID(value);
@@ -20,6 +19,11 @@ public class TimeZoneUtils {
     }
 
     private static boolean isBadTz(String value) {
-    	return BAD_TZs.contains(value);
+    	try {
+		    BadTimezone.valueOf(value.replaceAll("/", "_"));
+		    return true;
+	    } catch (IllegalArgumentException e) {
+    		return false;
+	    }
 	}
 }
