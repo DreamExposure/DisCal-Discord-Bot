da585fd371986798e235bfdf910505c5e22a7c0e
NovaFox161 <novamaday@gmail.com>
2018-01-03 07:00:07 -0600

    Start work on shortening event IDs

--- src/main/java/com/cloudcraftgaming/discal/api/crypto/KeyGenerator.java
+++ src/main/java/com/cloudcraftgaming/discal/api/crypto/KeyGenerator.java
@@ -11,6 +11,8 @@ import java.util.Random;
 public class KeyGenerator {
 	private static char[] VALID_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456879".toCharArray();
 
+	private static char[] VALID_CHARS_2 = "abcdefghijklmnopqrstuv0123456789".toCharArray();
+
 	// cs = cryptographically secure
 	@SuppressWarnings("SameParameterValue")
 	public static String csRandomAlphaNumericString(int numChars) {
@@ -26,5 +28,35 @@ public class KeyGenerator {
 			buff[i] = VALID_CHARACTERS[rand.nextInt(VALID_CHARACTERS.length)];
 		}
 		return new String(buff);
+	}
+
+	public static String generateEventId() {
+		SecureRandom secRand = new SecureRandom();
+		Random rand = new Random();
+		char[] buff = new char[9];
+
+		for (int i = 0; i < 9; ++i) {
+			// reseed rand once you've used up all available entropy bits
+			if ((i % 10) == 0) {
+				rand.setSeed(secRand.nextLong()); // 64 bits of random!
+			}
+			buff[i] = VALID_CHARS_2[rand.nextInt(VALID_CHARS_2.length)];
+		}
+		return "e" + new String(buff);
+	}
+
+	public static String generateAnnouncementId() {
+		SecureRandom secRand = new SecureRandom();
+		Random rand = new Random();
+		char[] buff = new char[9];
+
+		for (int i = 0; i < 9; ++i) {
+			// reseed rand once you've used up all available entropy bits
+			if ((i % 10) == 0) {
+				rand.setSeed(secRand.nextLong()); // 64 bits of random!
+			}
+			buff[i] = VALID_CHARS_2[rand.nextInt(VALID_CHARS_2.length)];
+		}
+		return "a" + new String(buff);
 	}
 }
