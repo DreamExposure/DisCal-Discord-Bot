9d89cf19ea3d7543ee391ec352313a0aeced6a4a
NovaFox161 <novamaday@gmail.com>
2018-01-23 10:26:22 -0600

    Remove JS that is no longer needed.

--- src/main/resources/web/public/scripts/dashboard/main.js
+++
@@ -1,7 +0,0 @@
-function useSimpleAnnouncements(useSimple) {
-	window.location.href = "/api/v1/dashboard/update/settings?simple-ann=" + useSimple;
-}
-
-function enableBranding(useBranding) {
-	window.location.href = "/api/v1/dashboard/update/settings?branding=" + useBranding;
-}
