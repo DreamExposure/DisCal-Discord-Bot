746f564c3b6b9fa3cbe44855e08713dd44921870
NovaFox161 <novamaday@gmail.com>
2018-03-06 12:27:02 -0600

    Calendar date displays work

--- src/main/resources/web/public/scripts/dashboard/calendar.js
+++ src/main/resources/web/public/scripts/dashboard/calendar.js
@@ -5,31 +5,95 @@ var calendar = {
 
 
 function getMonthName(index) {
-	return ['January', 'February',
-		'March', 'April',
-		'May', 'June',
-		'July', 'August',
-		'September', 'October',
-		'November', 'December'][index];
+	return ["January", "February",
+		"March", "April",
+		"May", "June",
+		"July", "August",
+		"September", "October",
+		"November", "December"][index];
 }
 
-function init() {
-	setMonth({date: calendar.todaysDate});
+function getDayName(index) {
+	return ["Sunday", "Monday",
+		"Tuesday", "Wednesday",
+		"Thursday", "Friday",
+		"Saturday"][index];
+}
+
+function dateDisplays() {
+	return ["r1c1", "r1c2", "r1c3", "r1c4", "r1c5", "r1c6", "r1c7",
+		"r2c1", "r2c2", "r2c3", "r2c4", "r2c5", "r2c6", "r2c7",
+		"r3c1", "r3c2", "r3c3", "r3c4", "r3c5", "r3c6", "r3c7",
+		"r4c1", "r4c2", "r4c3", "r4c4", "r4c5", "r4c6", "r4c7",
+		"r5c1", "r5c2", "r5c3", "r5c4", "r5c5", "r5c6", "r5c7",
+		"r6c1", "r6c2", "r6c3", "r6c4", "r6c5", "r6c6", "r6c7"];
+}
+
+function dateDisplaysToChange(str) {
+	return dateDisplays().slice(dateDisplays().indexOf(str), dateDisplays().length - 1);
+}
+
+function findFirstDayOfMonthPosition() {
+	var firstDay = new Date(calendar.selectedDate.getFullYear(), calendar.selectedDate.getMonth(), 1);
+
+	var firstDayName = getDayName(firstDay.getDay());
+
+	switch (firstDayName) {
+		case "Sunday":
+			return "r1c1";
+		case "Monday":
+			return "r1c2";
+		case "Tuesday":
+			return "r1c3";
+		case "Wednesday":
+			return "r1c4";
+		case "Thursday":
+			return "r1c5";
+		case "Friday":
+			return "r1c6";
+		case "Saturday":
+			return "r1c7";
+	}
+}
+
+function daysInMonth() {
+	return new Date(calendar.selectedDate.getFullYear(), calendar.selectedDate.getMonth() + 1, 0).getDate();
 }
 
 function setMonth(parameters) {
 	var date = parameters.date;
 
-	document.getElementById("month-display").innerHTML = getMonthName(date.getMonth()) + ' ' + date.getFullYear();
+	document.getElementById("month-display").innerHTML = getMonthName(date.getMonth()) + " " + date.getFullYear();
+
+	var tcc = dateDisplays();
+	for (var ii = 0; ii < tcc.length; ii++) {
+		document.getElementById(tcc[ii]).innerHTML = "";
+	}
+
+	var tc = dateDisplaysToChange(findFirstDayOfMonthPosition());
+	var count = daysInMonth();
+	for (var i = 0; i < tc.length; i++) {
+		var d = i + 1;
+		if (d <= count) {
+			document.getElementById(tc[i]).innerHTML = d + "";
+		}
+	}
 }
 
 function previousMonth() {
 	calendar.selectedDate.setMonth(calendar.selectedDate.getMonth() - 1);
+	calendar.selectedDate.setDate(1);
 
 	setMonth({date: calendar.selectedDate});
 }
 
 function nextMonth() {
 	calendar.selectedDate.setMonth(calendar.selectedDate.getMonth() + 1);
+	calendar.selectedDate.setDate(1);
+
 	setMonth({date: calendar.selectedDate});
+}
+
+function init() {
+	setMonth({date: calendar.todaysDate});
 }
