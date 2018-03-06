917b818bdb257c0f6978535ae14807002ebfaf11
NovaFox161 <novamaday@gmail.com>
2018-03-06 10:43:41 -0600

    Implement month scrolling

--- src/main/resources/web/public/scripts/dashboard/calendar.js
+++ src/main/resources/web/public/scripts/dashboard/calendar.js
@@ -1,3 +1,9 @@
+var calendar = {
+	todaysDate: new Date(),
+	selectedDate: new Date()
+};
+
+
 function getMonthName(index) {
 	return ['January', 'February',
 		'March', 'April',
@@ -8,13 +14,22 @@ function getMonthName(index) {
 }
 
 function init() {
-	var today = new Date();
-
-	setMonth({date: today});
+	setMonth({date: calendar.todaysDate});
 }
 
 function setMonth(parameters) {
 	var date = parameters.date;
 
 	document.getElementById("month-display").innerHTML = getMonthName(date.getMonth()) + ' ' + date.getFullYear();
+}
+
+function previousMonth() {
+	calendar.selectedDate.setMonth(calendar.selectedDate.getMonth() - 1);
+
+	setMonth({date: calendar.selectedDate});
+}
+
+function nextMonth() {
+	calendar.selectedDate.setMonth(calendar.selectedDate.getMonth() + 1);
+	setMonth({date: calendar.selectedDate});
 }
