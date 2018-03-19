030bda2598bed5422bfb6ba9998f9456bbc7ba6f
NovaFox161 <novamaday@gmail.com>
2018-03-19 13:06:47 -0500

    Fixing old, overlooked code

--- src/main/java/com/cloudcraftgaming/discal/bot/internal/consolecommand/ConsoleCommandExecutor.java
+++ src/main/java/com/cloudcraftgaming/discal/bot/internal/consolecommand/ConsoleCommandExecutor.java
@@ -1,6 +1,7 @@
 package com.cloudcraftgaming.discal.bot.internal.consolecommand;
 
 import com.cloudcraftgaming.discal.Main;
+import com.cloudcraftgaming.discal.api.object.BotSettings;
 import com.cloudcraftgaming.discal.bot.internal.service.ApplicationHandler;
 
 /**
@@ -13,38 +14,40 @@ public class ConsoleCommandExecutor {
 	 * Initiates the listener for commands via the DisCal console window.
 	 */
 	public static void init() {
-		while (true) {
-			System.out.println("Enter a command below: ");
-			String input = System.console().readLine();
-			Boolean cmdValid = false;
-			if (input != null && !input.isEmpty()) {
+		if (BotSettings.ACCEPT_CLI.get().equalsIgnoreCase("true")) {
+			while (true) {
+				System.out.println("Enter a command below: ");
+				String input = System.console().readLine();
+				Boolean cmdValid = false;
+				if (input != null && !input.isEmpty()) {
 
-				//Important commands first.
-				if (input.equalsIgnoreCase("exit")) {
-					ApplicationHandler.exitApplication();
-					return;
-				}
-				if (input.equalsIgnoreCase("restart")) {
-					ApplicationHandler.restartApplication(null);
-					return;
-				}
-				if (input.equalsIgnoreCase("?")) {
-					cmdValid = true;
-					System.out.println("Valid console commands: ");
-					System.out.println("exit");
-					System.out.println("restart");
-					System.out.println("serverCount");
-					System.out.println("silence true/false");
-					System.out.println();
-				}
-				if (input.startsWith("serverCount")) {
-					cmdValid = true;
-					System.out.println("Server count: " + Main.client.getGuilds().size());
-				}
+					//Important commands first.
+					if (input.equalsIgnoreCase("exit")) {
+						ApplicationHandler.exitApplication();
+						return;
+					}
+					if (input.equalsIgnoreCase("restart")) {
+						ApplicationHandler.restartApplication(null);
+						return;
+					}
+					if (input.equalsIgnoreCase("?")) {
+						cmdValid = true;
+						System.out.println("Valid console commands: ");
+						System.out.println("exit");
+						System.out.println("restart");
+						System.out.println("serverCount");
+						System.out.println("silence true/false");
+						System.out.println();
+					}
+					if (input.startsWith("serverCount")) {
+						cmdValid = true;
+						System.out.println("Server count: " + Main.client.getGuilds().size());
+					}
 
-				if (!cmdValid) {
-					System.out.println("Command not found! Use ? to list all commands.");
-					System.out.println();
+					if (!cmdValid) {
+						System.out.println("Command not found! Use ? to list all commands.");
+						System.out.println();
+					}
 				}
 			}
 		}
