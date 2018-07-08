ccbd2513665ef2cf51c294182b6c736bcfc96017
NovaFox161 <novamaday@gmail.com>
2018-07-08 18:57:00 -0500

    Use OkHTTP instead of Unirest for Discord login auth on dashboard.

--- src/main/java/com/cloudcraftgaming/discal/web/network/discord/DiscordLoginHandler.java
+++ src/main/java/com/cloudcraftgaming/discal/web/network/discord/DiscordLoginHandler.java
@@ -7,9 +7,9 @@ import com.cloudcraftgaming.discal.api.object.BotSettings;
 import com.cloudcraftgaming.discal.api.utils.GuildUtils;
 import com.cloudcraftgaming.discal.logger.Logger;
 import com.cloudcraftgaming.discal.web.handler.DiscordAccountHandler;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 import org.json.JSONException;
 import org.json.JSONObject;
 import spark.Request;
@@ -30,19 +30,55 @@ import static spark.Spark.halt;
 public class DiscordLoginHandler {
 
 	public static String handleDiscordCode(Request request, Response response) {
+		OkHttpClient client = new OkHttpClient();
+
 		try {
 			String code = request.queryParams("code");
 
+			RequestBody body = new FormBody.Builder()
+					.add("client_id", BotSettings.ID.get())
+					.add("client_secret", BotSettings.SECRET.get())
+					.add("grant_type", "authorization_code")
+					.add("code", code)
+					.add("redirect_uri", BotSettings.REDIR_URL.get())
+					.build();
+
+			okhttp3.Request httpRequest = new okhttp3.Request.Builder()
+					.url("https://discordapp.com/api/v6/oauth2/token")
+					.post(body)
+					.header("Content-Type", "application/x-www-form-urlencoded")
+					.build();
+
 			//POST request to discord for access...
+			/*
 			HttpResponse<JsonNode> httpResponse = Unirest.post("https://discordapp.com/api/v6/oauth2/token").header("Content-Type", "application/x-www-form-urlencoded").field("client_id", BotSettings.ID.get()).field("client_secret", BotSettings.SECRET.get()).field("grant_type", "authorization_code").field("code", code).field("redirect_uri", BotSettings.REDIR_URL.get()).asJson();
 
 			JSONObject info = new JSONObject(httpResponse.getBody()).getJSONObject("object");
+			*/
+
+			okhttp3.Response httpResponse = client.newCall(httpRequest).execute();
+
+			@SuppressWarnings("ConstantConditions")
+			JSONObject info = new JSONObject(httpResponse.body()).getJSONObject("object");
 
 			if (info.has("access_token")) {
 				//GET request for user info...
+				okhttp3.Request userDataRequest = new okhttp3.Request.Builder()
+						.url("https://discordapp.com/api/v6/users/@me")
+						.header("Authorization", "Bearer " + info.getString("access_token"))
+						.build();
+
+				okhttp3.Response userDataResponse = client.newCall(userDataRequest).execute();
+
+				/*
 				HttpResponse<JsonNode> userDataResponse = Unirest.get("https://discordapp.com/api/v6/users/@me").header("Authorization", "Bearer " + info.getString("access_token")).asJson();
 
 				JSONObject userInfo = new JSONObject(userDataResponse.getBody()).getJSONObject("object");
+
+				 */
+
+				@SuppressWarnings("ConstantConditions")
+				JSONObject userInfo = new JSONObject(userDataResponse.body()).getJSONObject("object");
 
 				//Saving session info and access info to memory until moved into the database...
 				Map m = new HashMap();
