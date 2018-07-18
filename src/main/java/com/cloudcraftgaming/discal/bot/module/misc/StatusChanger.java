1aba61b98a00717e37287afde9b1c17cd6a563d9
NovaFox161 <novamaday@gmail.com>
2018-07-17 21:55:19 -0500

    Fix some branding based stuff as directed by Discord

--- src/main/java/com/cloudcraftgaming/discal/bot/module/misc/StatusChanger.java
+++ src/main/java/com/cloudcraftgaming/discal/bot/module/misc/StatusChanger.java
@@ -28,7 +28,7 @@ public class StatusChanger extends TimerTask {
         statuses.add("!help for help");
         statuses.add("!DisCal for info");
         statuses.add("Made by NovaFox161");
-        statuses.add("Official calendar bot!");
+		statuses.add("Best calendar bot!");
         statuses.add("Used on %guCount% guilds!");
         statuses.add("%calCount% calendars!");
         statuses.add("%annCount% announcements!");
