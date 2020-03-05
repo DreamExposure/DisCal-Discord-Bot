a986afdad6a06011584aa56cced634a2654db6dd
NovaFox161 <novamaday@gmail.com>
2020-03-05 14:11:25 -0600

    Actually retrieve the url from settings

--- client/src/main/java/org/dreamexposure/discal/client/service/KeepAliveHandler.java
+++ client/src/main/java/org/dreamexposure/discal/client/service/KeepAliveHandler.java
@@ -54,7 +54,7 @@ public class KeepAliveHandler {
 
 					RequestBody body = RequestBody.create(GlobalConst.JSON, data.toString());
 					Request request = new Request.Builder()
-							.url(BotSettings.API_URL + "/v2/status/keep-alive")
+							.url(BotSettings.API_URL.get() + "/v2/status/keep-alive")
 							.post(body)
 							.header("Authorization", BotSettings.BOT_API_TOKEN.get())
 							.header("Content-Type", "application/json")
