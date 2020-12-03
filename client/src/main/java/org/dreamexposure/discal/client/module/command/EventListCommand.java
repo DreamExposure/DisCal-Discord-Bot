a7b75bcf560f504c08088f8e2eb209a9eb841216
NovaFox161 <novamaday@gmail.com>
2020-12-02 21:50:49 -0600

    Add endpoint for ongoing events, fix language in ongoing command

--- client/src/main/java/org/dreamexposure/discal/client/module/command/EventListCommand.java
+++ client/src/main/java/org/dreamexposure/discal/client/module/command/EventListCommand.java
@@ -1,7 +1,7 @@
 package org.dreamexposure.discal.client.module.command;
 
 import com.google.api.services.calendar.model.Event;
-import discord4j.core.event.domain.message.MessageCreateEvent;
+
 import org.dreamexposure.discal.client.message.EventMessageFormatter;
 import org.dreamexposure.discal.client.message.Messages;
 import org.dreamexposure.discal.core.database.DatabaseManager;
@@ -9,12 +9,14 @@ import org.dreamexposure.discal.core.object.GuildSettings;
 import org.dreamexposure.discal.core.object.command.CommandInfo;
 import org.dreamexposure.discal.core.utils.GlobalConst;
 import org.dreamexposure.discal.core.wrapper.google.EventWrapper;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by Nova Fox on 1/3/2017.
@@ -189,17 +191,18 @@ public class EventListCommand implements Command {
     }
 
     @SuppressWarnings("DuplicatedCode")
+    //TODO: Add translations for new text values
     private Mono<Void> moduleOngoing(String[] args, MessageCreateEvent event, GuildSettings settings) {
         return Mono.defer(() -> {
             if (args.length == 1) {
                 return DatabaseManager.getMainCalendar(settings.getGuildID()) //TODO: Support multi-cal
                     .flatMap(data -> {
                         final long start = System.currentTimeMillis() - (GlobalConst.oneDayMs * 14); // 2 weeks ago
-                        final long now = System.currentTimeMillis() + GlobalConst.oneDayMs; // one day from now
+                        final long end = System.currentTimeMillis() + GlobalConst.oneDayMs; // one day from now
 
-                        return EventWrapper.getEvents(data, settings, start, now).flatMap(events -> {
+                        return EventWrapper.getEvents(data, settings, start, end).flatMap(events -> {
                             if (events.isEmpty()) {
-                                return Messages.sendMessage(Messages.getMessage("Event.List.Found.None", settings), event);
+                                return Messages.sendMessage("No ongoing events found!", event);
                             } else {
                                 //Filter through and check if they are currently on-going.
                                 List<Event> ongoing = events.stream()
@@ -209,17 +212,17 @@ public class EventListCommand implements Command {
 
                                 //Filtered...
                                 if (ongoing.isEmpty()) {
-                                    return Messages.sendMessage(Messages.getMessage("Event.List.Found.None", settings), event);
+                                    return Messages.sendMessage("No ongoing events found!", event);
                                 } else if (ongoing.size() == 1) {
                                     return EventMessageFormatter
                                         .getEventEmbed(ongoing.get(0), data.getCalendarNumber(), settings).flatMap(embed ->
-                                            Messages.sendMessage(
-                                                Messages.getMessage("Event.List.Found.One", settings), embed, event));
+                                            Messages.sendMessage("One ongoing event found", embed, event));
                                 } else {
-                                    return Messages.sendMessage(Messages.getMessage("Event.List.Found.Many", "%amount%",
-                                        ongoing.size() + "", settings), event).then(Flux.fromIterable(ongoing)
-                                        .concatMap(e -> EventMessageFormatter.getCondensedEventEmbed(e, data.getCalendarNumber(), settings)
-                                            .flatMap(embed -> Messages.sendMessage(embed, event))).then())
+                                    return Messages.sendMessage(ongoing.size() + " ongoing events found." +
+                                        " It make take some time to list them all...", event)
+                                        .then(Flux.fromIterable(ongoing)
+                                            .concatMap(e -> EventMessageFormatter.getCondensedEventEmbed(e, data.getCalendarNumber(), settings)
+                                                .flatMap(embed -> Messages.sendMessage(embed, event))).then())
                                         .thenReturn(GlobalConst.NOT_EMPTY);
                                 }
                             }
