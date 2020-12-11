adb991cea642f2dfe999fef49103734aa3fa0c3a
NovaFox161 <novamaday@gmail.com>
2020-12-11 08:15:05 -0600

    Fix malformed SQL

--- core/src/main/resources/db/migration/V9__Publishable_Announcements.sql
+++ core/src/main/resources/db/migration/V9__Publishable_Announcements.sql
@@ -1,5 +1,5 @@
 # noinspection SqlResolveForFile
 
 ALTER TABLE `${prefix}announcements`
-    ADD COLUMN PUBLISH BIT(1) NOT NULL DEFAULT '0'
+    ADD COLUMN PUBLISH BIT(1) NOT NULL DEFAULT 0
         AFTER INFO_ONLY;
