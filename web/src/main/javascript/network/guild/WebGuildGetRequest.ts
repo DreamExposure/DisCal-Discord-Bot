5502c0ca5b0a5060b34f42267e1eac77575b04ac
NovaFox161 <novamaday@gmail.com>
2021-02-01 12:02:02 -0600

    Working on getting web code updated to new data structures

--- web/src/main/javascript/network/guild/WebGuildGetRequest.ts
+++ web/src/main/javascript/network/guild/WebGuildGetRequest.ts
@@ -5,62 +5,63 @@ import {TaskType} from "@/enums/TaskType";
 import jqXHR = JQuery.jqXHR;
 
 export class WebGuildGetRequest implements AsyncTask {
-	private readonly guildId: string;
-	private readonly userId: string;
+    private readonly guildId: string;
+    private readonly userId: string;
 
-	readonly callback: TaskCallback;
+    readonly callback: TaskCallback;
 
-	apiKey: string = "";
-	apiUrl: string = "";
+    apiKey: string = "";
+    apiUrl: string = "";
 
 
-	constructor(guildId: string, userId: string, callback: TaskCallback) {
-		this.guildId = guildId;
-		this.userId = userId;
-		this.callback = callback;
-	}
+    constructor(guildId: string, userId: string, callback: TaskCallback) {
+        this.guildId = guildId;
+        this.userId = userId;
+        this.callback = callback;
+    }
 
-	provideApiDetails(apiKey: string, apiUrl: string): void {
-		this.apiKey = apiKey;
-		this.apiUrl = apiUrl;
-	}
+    provideApiDetails(apiKey: string, apiUrl: string): void {
+        this.apiKey = apiKey;
+        this.apiUrl = apiUrl;
+    }
 
-	execute(): void {
-		let bodyRaw = {
-			"guild_id": this.guildId,
-			"user_id": this.userId
-		};
+    execute(): void {
+        let bodyRaw = {
+            "guild_id": this.guildId,
+            "user_id": this.userId
+        };
 
-		$.ajax({
-			url: this.apiUrl + "/v2/guild/get",
-			headers: {
-				"Content-Type": "application/json",
-				"Authorization": this.apiKey
-			},
-			method: "POST",
-			dataType: "json",
-			data: JSON.stringify(bodyRaw),
-			success: function (json: any) {
-				let status = new NetworkCallStatus(true, TaskType.WEB_GUILD_GET);
-				status.code = 200;
-				status.body = json;
-				status.message = "Success";
+        $.ajax({
+            url: this.apiUrl + "/v2/guild/get",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": this.apiKey
+            },
+            method: "POST",
+            dataType: "json",
+            data: JSON.stringify(bodyRaw),
+            success: function (json: any) {
+                let status = new NetworkCallStatus(true, TaskType.WEB_GUILD_GET);
+                status.code = 200;
+                status.body = json;
+                status.message = "Success";
 
-				this.onComplete(status);
+                this.onComplete(status);
 
-			}.bind(this),
-			error: function (jqXHR: jqXHR) {
-				let status = new NetworkCallStatus(false, TaskType.WEB_GUILD_GET);
-				status.code = jqXHR.status;
-				status.body = jqXHR.responseJSON;
-				status.message = jqXHR.responseJSON.message;
+            }.bind(this),
+            error: function (jqXHR: jqXHR) {
+                console.log("WebGuildGetRequest err: " + jqXHR);
+                let status = new NetworkCallStatus(false, TaskType.WEB_GUILD_GET);
+                status.code = jqXHR.status;
+                status.body = jqXHR.responseJSON;
+                status.message = jqXHR.responseJSON.message;
 
-				this.onComplete(status);
-			}.bind(this)
-		});
-	}
+                this.onComplete(status);
+            }.bind(this)
+        });
+    }
 
-	onComplete(status: NetworkCallStatus): void {
-		this.callback.onCallback(status);
-	}
+    onComplete(status: NetworkCallStatus): void {
+        this.callback.onCallback(status);
+    }
 }
--- web/src/main/javascript/objects/guild/WebGuild.ts
+++ web/src/main/javascript/objects/guild/WebGuild.ts
@@ -5,49 +5,49 @@ import {Announcement} from "@/objects/announcement/Announcement";
 import {WebCalendar} from "@/objects/calendar/WebCalendar";
 
 export class WebGuild {
-	private readonly _id: string;
-	private _name: string = "";
-	private _icon: string = "";
+    private readonly _id: string;
+    private _name: string = "";
+    private _icon: string = "";
 
-	private _settings: GuildSettings = new GuildSettings("");
-	private _botNick: string = "";
+    private _settings: GuildSettings = new GuildSettings("");
+    private _botNick: string = "";
 
-	private _canManageServer: boolean = false;
-	private _hasDisCalRole: boolean = false;
+    private _canManageServer: boolean = false;
+    private _hasDisCalRole: boolean = false;
 
-	private readonly _roles: WebRole[] = [];
-	private readonly _channels: WebChannel[] = [];
-	private readonly _announcements: Announcement[] = [];
-	private readonly _availableLangs: string[] = [];
+    private readonly _roles: WebRole[] = [];
+    private readonly _channels: WebChannel[] = [];
+    private readonly _announcements: Announcement[] = [];
+    private readonly _availableLangs: string[] = [];
 
-	private _calendar: WebCalendar = new WebCalendar();
+    private _calendar: WebCalendar = new WebCalendar();
 
-	private _shard: number = 0;
+    private _shard: number = 0;
 
-	constructor(id: string) {
-		this._id = id;
-	}
+    constructor(id: string) {
+        this._id = id;
+    }
 
-	//Getter/Setter pairs
-	get id() {
-		return this._id;
-	}
+    //Getter/Setter pairs
+    get id() {
+        return this._id;
+    }
 
-	get name() {
-		return this._name;
-	}
+    get name() {
+        return this._name;
+    }
 
-	set name(name) {
-		this._name = name;
-	}
+    set name(name) {
+        this._name = name;
+    }
 
-	get icon() {
-		return this._icon;
-	}
+    get icon() {
+        return this._icon;
+    }
 
-	set icon(iconUrl) {
-		this._icon = iconUrl;
-	}
+    set icon(iconUrl) {
+        this._icon = iconUrl;
+    }
 
     get settings() {
         return this._settings;
@@ -75,86 +75,86 @@ export class WebGuild {
 
     get hasDisCalRole() {
         return this._hasDisCalRole;
-	}
+    }
 
-	set hasDisCalRole(hasRole) {
-		this._hasDisCalRole = hasRole;
-	}
+    set hasDisCalRole(hasRole) {
+        this._hasDisCalRole = hasRole;
+    }
 
-	get roles() {
-		return this._roles;
-	}
+    get roles() {
+        return this._roles;
+    }
 
-	get channels() {
-		return this._channels;
-	}
+    get channels() {
+        return this._channels;
+    }
 
-	get announcements() {
-		return this._announcements;
-	}
+    get announcements() {
+        return this._announcements;
+    }
 
-	get availableLangs() {
-		return this._availableLangs;
-	}
+    get availableLangs() {
+        return this._availableLangs;
+    }
 
-	get calendar() {
-		return this._calendar;
-	}
+    get calendar() {
+        return this._calendar;
+    }
 
-	set calendar(calendar) {
-		this._calendar = calendar;
-	}
+    set calendar(calendar) {
+        this._calendar = calendar;
+    }
 
-	get shard() {
-		return this._shard;
-	}
+    get shard() {
+        return this._shard;
+    }
 
-	set shard(shard) {
-		this._shard = shard;
-	}
+    set shard(shard) {
+        this._shard = shard;
+    }
 
-	//Json conversions
-	toJson() {
-		let json: any = {
-			"id": this.id,
-			"name": this.name,
-			"settings": this.settings.toJson(),
-			"manage_server": this.canManageServer,
-			"discal_role": this.hasDisCalRole,
-			"calendar": this.calendar.toJson()
-		};
-		if (this.icon != null) {
-			json.icon = this.icon;
-		}
-		if (this.botNick != null && this.botNick !== "") {
-			json.bot_nick = this.botNick;
-		}
+    //Json conversions
+    toJson() {
+        let json: any = {
+            "id": this.id,
+            "name": this.name,
+            "settings": this.settings.toJson(),
+            "manage_server": this.canManageServer,
+            "discal_role": this.hasDisCalRole,
+            "calendar": this.calendar.toJson()
+        };
+        if (this.icon != null) {
+            json.icon = this.icon;
+        }
+        if (this.botNick != null && this.botNick !== "") {
+            json.bot_nick = this.botNick;
+        }
 
-		let jRoles = [];
-		for (let i = 0; i < this.roles.length; i++) {
-			let role = this.roles[i];
-			jRoles.push(role.toJson());
-		}
-		json.roles = jRoles;
+        let jRoles = [];
+        for (let i = 0; i < this.roles.length; i++) {
+            let role = this.roles[i];
+            jRoles.push(role.toJson());
+        }
+        json.roles = jRoles;
 
-		let jChannels = [];
-		for (let i = 0; i < this.channels.length; i++) {
-			let channel = this.channels[i];
-			jChannels.push(channel.toJson());
-		}
-		json.channels = jChannels;
+        let jChannels = [];
+        for (let i = 0; i < this.channels.length; i++) {
+            let channel = this.channels[i];
+            jChannels.push(channel.toJson());
+        }
+        json.channels = jChannels;
 
-		let jAnnouncements = [];
-		for (let i = 0; i < this.announcements.length; i++) {
-			let announcement = this.announcements[i];
-			jAnnouncements.push(announcement.toJson());
-		}
-		json.announcements = jAnnouncements;
+        let jAnnouncements = [];
+        for (let i = 0; i < this.announcements.length; i++) {
+            let announcement = this.announcements[i];
+            jAnnouncements.push(announcement.toJson());
+        }
+        json.announcements = jAnnouncements;
 
-		json.shard = this.shard;
+        json.shard = this.shard;
 
-		return json;
-	}
+        return json;
+    }
 
     fromJson(json: any) {
         //We don't need to set ID, this is already done...
@@ -174,25 +174,25 @@ export class WebGuild {
         this.hasDisCalRole = json.discal_role;
 
         for (let i = 0; i < json.roles.length; i++) {
-			this.roles.push(new WebRole().fromJson(json.roles[i]));
-		}
+            this.roles.push(new WebRole().fromJson(json.roles[i]));
+        }
 
-		for (let i = 0; i < json.channels.length; i++) {
-			this.channels.push(new WebChannel().fromJson(json.channels[i]));
-		}
+        for (let i = 0; i < json.channels.length; i++) {
+            this.channels.push(new WebChannel().fromJson(json.channels[i]));
+        }
 
-		for (let i = 0; i < json.announcements.length; i++) {
-			this.roles.push(new WebRole().fromJson(json.roles[i]));
-		}
+        for (let i = 0; i < json.announcements.length; i++) {
+            this.roles.push(new WebRole().fromJson(json.roles[i]));
+        }
 
-		for (let i = 0; i < json.available_langs.length; i++) {
-			this.availableLangs.push(json.available_langs[i]);
-		}
+        for (let i = 0; i < json.available_langs.length; i++) {
+            this.availableLangs.push(json.available_langs[i]);
+        }
 
-		this.calendar = new WebCalendar().fromJson(json.calendar);
+        this.calendar = new WebCalendar().fromJson(json.calendar);
 
-		this.shard = json.shard;
+        this.shard = json.shard;
 
-		return this;
-	}
+        return this;
+    }
 }
