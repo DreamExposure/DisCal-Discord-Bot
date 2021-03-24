4abc1b70d7b413d14254bf7461d1ab50d3584d27
NovaFox161 <novamaday@gmail.com>
2021-03-24 16:17:45 -0500

    Start work on adding in Entities for abstraction

---
+++ core/src/main/kotlin/org/dreamexposure/discal/core/entities/event/GoogleEvent.kt
@@ -0,0 +1,40 @@
+package org.dreamexposure.discal.core.entities.event
+
+import discord4j.common.util.Snowflake
+import org.dreamexposure.discal.core.`object`.announcement.Announcement
+import org.dreamexposure.discal.core.`object`.event.Recurrence
+import org.dreamexposure.discal.core.`object`.event.RsvpData
+import org.dreamexposure.discal.core.entities.Event
+import org.dreamexposure.discal.core.enums.event.EventColor
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.time.Instant
+
+class GoogleEvent(override val eventId: String, override val guildId: Snowflake) : Event {
+
+    override var name: String = ""
+    override var description: String = ""
+    override var location: String = ""
+
+    override var color: EventColor = EventColor.NONE
+
+    override lateinit var start: Instant
+    override lateinit var end: Instant
+
+    override var recur: Boolean = false
+    override var recurrence: Recurrence = Recurrence()
+
+    override var image: String = ""
+
+    override fun getTimezone(): Mono<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLinkedAnnouncements(): Flux<Announcement> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRsvp(): Mono<RsvpData> {
+        TODO("Not yet implemented")
+    }
+}
