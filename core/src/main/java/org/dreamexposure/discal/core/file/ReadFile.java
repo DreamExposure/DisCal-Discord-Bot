bd9020fb13f513480ba2fefc1ca5adf52d518f60
NovaFox161 <novamaday@gmail.com>
2021-06-24 17:09:36 -0500

    Fix issues reading lang files from resources

--- core/src/main/java/org/dreamexposure/discal/core/file/ReadFile.java
+++ core/src/main/java/org/dreamexposure/discal/core/file/ReadFile.java
@@ -1,17 +1,16 @@
 package org.dreamexposure.discal.core.file;
 
+import kotlin.text.Charsets;
 import org.dreamexposure.discal.core.logger.LogFeed;
 import org.dreamexposure.discal.core.logger.object.LogObject;
 import org.json.JSONObject;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.util.FileCopyUtils;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
-import java.io.File;
-import java.io.FileReader;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
 
 /**
  * Created by Nova Fox on 11/10/17.
@@ -19,36 +18,32 @@ import java.nio.file.Paths;
  * For Project: DisCal-Discord-Bot
  */
 public class ReadFile {
-    @SuppressWarnings({"resource", "IOResourceOpenedButNotSafelyClosed"})
     public static Mono<JSONObject> readAllLangFiles() {
         return Mono.fromCallable(() -> {
             final JSONObject langs = new JSONObject();
 
             try {
-                for (final File file : getResourceFolderFiles("languages")) {
+                var pathMatching = new PathMatchingResourcePatternResolver();
+
+                for (Resource res : pathMatching.getResources("languages/*.json")) {
+                    var reader = new InputStreamReader(res.getInputStream(), Charsets.UTF_8);
+
                     // Open the file
-                    final FileReader fr = new FileReader(file);
+                    final String contents = FileCopyUtils.copyToString(reader);
 
-                    final byte[] encoded = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
-                    final String contents = new String(encoded, StandardCharsets.UTF_8);
+                    //Close reader
+                    reader.close();
 
+                    //Parse json
                     final JSONObject json = new JSONObject(contents);
 
-                    langs.put(json.getString("Language"), json);
-
-                    fr.close();
+                    if (!json.getString("Language").equalsIgnoreCase("TEMPLATE"))
+                        langs.put(json.getString("Language"), json);
                 }
             } catch (final Exception e) {
                 LogFeed.log(LogObject.forException("Failed to load lang files", e, ReadFile.class));
             }
             return langs;
         }).subscribeOn(Schedulers.boundedElastic());
-    }
-
-    private static File[] getResourceFolderFiles(String folder) {
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        URL url = loader.getResource(folder);
-        String path = url.getPath();
-        return new File(path).listFiles();
     }
 }
