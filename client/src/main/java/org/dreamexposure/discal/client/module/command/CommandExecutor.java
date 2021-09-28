21a8ecc41f3d0e4550c132d1b57259ff71797011
NovaFox161 <novamaday@gmail.com>
2021-09-28 18:23:02 -0500

    Add info to inform users of commands being converted to slash commands

--- client/src/main/java/org/dreamexposure/discal/client/module/command/CommandExecutor.java
+++ client/src/main/java/org/dreamexposure/discal/client/module/command/CommandExecutor.java
@@ -1,14 +1,15 @@
 package org.dreamexposure.discal.client.module.command;
 
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import org.dreamexposure.discal.client.message.Messages;
 import org.dreamexposure.discal.core.object.GuildSettings;
+import org.dreamexposure.discal.core.object.command.CommandInfo;
 import org.dreamexposure.discal.core.utils.GeneralUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import discord4j.core.event.domain.message.MessageCreateEvent;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 /**
  * Created by Nova Fox on 1/3/2017.
@@ -17,6 +18,10 @@ import reactor.core.publisher.Mono;
  */
 public class CommandExecutor {
     private static final List<Command> commands = new ArrayList<>();
+
+    private static final List<String> movedCommands = List.of(
+        "help", "discal", "time", "linkcal", "calendarlink", "callink", "linkcallador", "events", "rsvp", "dev"
+    );
 
     private CommandExecutor() {
     }
@@ -41,6 +46,8 @@ public class CommandExecutor {
             args = new String[0];
         }
 
+
+
         return Mono.from(getCommand(cmd)
             .flatMap(c -> c.issueCommand(args, event, settings)));
     }
@@ -63,11 +70,34 @@ public class CommandExecutor {
         return commands;
     }
 
-    public static Mono<Command> getCommand(final String cmdNameOrAlias) {
+    private static Mono<Command> getCommand(final String cmdNameOrAlias) {
         return Flux.fromIterable(commands)
             .filter(c ->
                 c.getCommand().equalsIgnoreCase(cmdNameOrAlias)
                     || c.getAliases().contains(cmdNameOrAlias.toLowerCase()))
             .next();
+    }
+}
+
+class WarningCommand implements Command {
+
+    @Override
+    public String getCommand() {
+        return "\u200Bdiscal-command-moved-warning";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public CommandInfo getCommandInfo() {
+        return new CommandInfo("", "", "");
+    }
+
+    @Override
+    public Mono<Void> issueCommand(String[] args, MessageCreateEvent event, GuildSettings settings) {
+        return Messages.sendMessage("This command has been moved. Please use the slash command version by typing `/`", event).then();
     }
 }
