ffc8b70a1142316bd711a65654e7881977d908b4
NovaFox161 <novamaday@gmail.com>
2021-10-15 23:05:18 -0500

    Fix bad SQL syntax

--- core/src/main/resources/db/migration/V21__Rename_Tables.sql
+++ core/src/main/resources/db/migration/V21__Rename_Tables.sql
@@ -1,10 +1,10 @@
 # noinspection SqlResolveForFile
 
-RENAME TABLE '${prefix}announcements' TO 'announcements',
-    '${prefix}api' TO 'api',
-    '${prefix}calendars' TO 'calendars',
-    '${prefix}credentials' TO 'credentials',
-    '${prefix}events' TO 'events',
-    '${prefix}guild_settings' TO 'guild_settings',
-    '${prefix}rsvp' TO 'rsvp';
+RENAME TABLE ${prefix}announcements TO announcements,
+    ${prefix}api TO api,
+    ${prefix}calendars TO calendars,
+    ${prefix}credentials TO credentials,
+    ${prefix}events TO events,
+    ${prefix}guild_settings TO guild_settings,
+    ${prefix}rsvp TO rsvp;
 
