dc710e2cabb4c7bc4967dc681a0472851989cc84
NovaFox161 <novamaday@gmail.com>
2021-11-19 17:20:45 -0600

    This should fix wizards not auto clearing after timeout

--- core/src/main/kotlin/org/dreamexposure/discal/core/object/Wizard.kt
+++ core/src/main/kotlin/org/dreamexposure/discal/core/object/Wizard.kt
@@ -1,11 +1,20 @@
 package org.dreamexposure.discal.core.`object`
 
 import discord4j.common.util.Snowflake
+import reactor.core.publisher.Flux
+import java.time.Duration
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 import java.util.concurrent.ConcurrentHashMap
 
 class Wizard<T: Pre> {
     private val active = ConcurrentHashMap<Snowflake, T>()
+
+    init {
+        Flux.interval(Duration.ofMinutes(30))
+                .map { removeOld() }
+                .subscribe()
+    }
 
     fun get(id: Snowflake): T? {
         val p = active[id]
@@ -16,4 +25,15 @@ class Wizard<T: Pre> {
     fun start(pre: T): T? = active.put(pre.guildId, pre)
 
     fun remove(id: Snowflake): T? = active.remove(id)
+
+    private fun removeOld() {
+        val toRemove = mutableListOf<Snowflake>()
+
+        active.forEach {
+            if (Instant.now().isAfter(it.value.lastEdit.plus(30, ChronoUnit.MINUTES))) {
+                toRemove.add(it.key)
+            }
+        }
+        toRemove.forEach { active.remove(it) }
+    }
 }
