6c39f27229c140ebfac8fc6cd31d2a9a639c4702
NovaFox161 <novamaday@gmail.com>
2022-03-25 11:36:42 -0500

    I can't believe this used to work lmao

--- core/src/main/kotlin/org/dreamexposure/discal/core/object/network/discal/NetworkData.kt
+++ core/src/main/kotlin/org/dreamexposure/discal/core/object/network/discal/NetworkData.kt
@@ -7,38 +7,42 @@ import kotlin.math.roundToInt
 
 @Serializable
 data class NetworkData(
-        @SerialName("total_calendars")
-        var totalCalendars: Int = 0,
+    @SerialName("total_calendars")
+    var totalCalendars: Int = 0,
 
-        @SerialName("total_announcements")
-        var totalAnnouncements: Int = 0,
+    @SerialName("total_announcements")
+    var totalAnnouncements: Int = 0,
 
-        @SerialName("api_status")
-        var apiStatus: InstanceData = InstanceData(),
+    @SerialName("api_status")
+    var apiStatus: InstanceData = InstanceData(),
 
-        @SerialName("website_status")
-        var websiteStatus: InstanceData? = null,
+    @SerialName("website_status")
+    var websiteStatus: InstanceData? = null,
 
-        @SerialName("cam_status")
-        var camStatus: InstanceData? = null,
+    @SerialName("cam_status")
+    var camStatus: InstanceData? = null,
 
-        @SerialName("bot_status")
-        val botStatus: MutableList<BotInstanceData> = CopyOnWriteArrayList()
+    @SerialName("bot_status")
+    val botStatus: MutableList<BotInstanceData> = CopyOnWriteArrayList()
 ) {
+    @Serializable
     @SerialName("expected_shard_count")
-    val expectedShardCount: Int
-    get() {
-        return if (botStatus.isNotEmpty()) botStatus[0].shardCount
-        else -1
-    }
+    var expectedShardCount: Int = -1
+        private set
+        get() {
+            return if (botStatus.isNotEmpty()) botStatus[0].shardCount
+            else field
+        }
 
+    @Serializable
     @SerialName("total_guilds")
-    val totalGuilds: Int
-    get() {
-        var guilds = 0
-        botStatus.forEach { guilds += it.guilds }
-        return guilds
-    }
+    var totalGuilds: Int = 0
+        private set
+        get() {
+            var guilds = 0
+            botStatus.forEach { guilds += it.guilds }
+            return guilds
+        }
 
     @Suppress("unused") //Used in thymeleaf status page
     fun getCurrentShardCount() = botStatus.size
@@ -51,7 +55,7 @@ data class NetworkData(
         totalMemMb += websiteStatus?.memory ?: 0.0
         totalMemMb += camStatus?.memory ?: 0.0
 
-        botStatus.forEach { totalMemMb+= it.instanceData.memory }
+        botStatus.forEach { totalMemMb += it.instanceData.memory }
 
         return ((totalMemMb / 1024) * 100).roundToInt().toDouble() / 100
     }
