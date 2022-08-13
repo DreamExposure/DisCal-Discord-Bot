139b5d88cd9a69758d31a237fc0057df17107a35
NovaFox161 <novamaday@gmail.com>
2022-08-13 14:54:03 -0500

    Attempting to resolve displaycal multi-day event bug

--- core/src/main/kotlin/org/dreamexposure/discal/core/extensions/MutableList.kt
+++ core/src/main/kotlin/org/dreamexposure/discal/core/extensions/MutableList.kt
@@ -37,7 +37,7 @@ fun MutableList<Event>.groupByDate(): Map<ZonedDateTime, List<Event>> {
 }
 
 // TODO: This could use some optimization, but we'll leave it for now
-fun MutableList<Event>.groupByDateMulti(): Map<ZonedDateTime, List<Event>> {
+fun MutableList<Event>.groupByDateMultiOld(): Map<ZonedDateTime, List<Event>> {
     // Each of the days events start on, their ending is ignored
     val dates = this.map {
         ZonedDateTime.ofInstant(it.start, it.timezone).truncatedTo(ChronoUnit.DAYS)
@@ -49,7 +49,7 @@ fun MutableList<Event>.groupByDateMulti(): Map<ZonedDateTime, List<Event>> {
     dates.forEach {
         LOGGER.debug("Date: $it")
 
-            val range = LongRange(it.toEpochSecond(), it.plusHours(23).plusMinutes(59).toEpochSecond())
+        val range = LongRange(it.toEpochSecond(), it.plusHours(23).plusMinutes(59).toEpochSecond())
         LOGGER.debug("Range: ${Instant.ofEpochSecond(range.first)} - ${Instant.ofEpochSecond(range.last)}")
 
         val events: MutableList<Event> = mutableListOf()
@@ -69,5 +69,45 @@ fun MutableList<Event>.groupByDateMulti(): Map<ZonedDateTime, List<Event>> {
         multi[it] = events
     }
 
+    return multi.toSortedMap()
+}
+
+fun MutableList<Event>.groupByDateMulti(): Map<ZonedDateTime, List<Event>> {
+    // First get a list of distinct dates each event starts on
+    val rawDates = this.map {
+        ZonedDateTime.ofInstant(it.start, it.timezone).truncatedTo(ChronoUnit.DAYS)
+            .with(TemporalAdjusters.ofDateAdjuster { identity -> identity })
+    }.toMutableList()
+
+    // Add multi-day end dates if not already present
+    rawDates += this.asSequence().filter {
+        it.isMultiDay()
+    }.map {
+        ZonedDateTime.ofInstant(it.end, it.timezone).truncatedTo(ChronoUnit.DAYS)
+            .with(TemporalAdjusters.ofDateAdjuster { identity -> identity })
+    }
+
+    // Sort dates
+    val sortedDates = rawDates.distinct().sorted().toList()
+
+    // Group events
+    val multi = mutableMapOf<ZonedDateTime, List<Event>>()
+
+    sortedDates.forEach {
+        val range = LongRange(it.toEpochSecond(), it.plusHours(23).plusMinutes(59).toEpochSecond())
+        val events = mutableListOf<Event>()
+
+        this.forEach { event ->
+            // When we check event end, we bump it back a second in order to prevent weirdness.
+            if (range.contains(event.start.epochSecond) || range.contains(event.end.epochSecond - 1)) {
+                // Event in range, add to list
+                events.add(event)
+            } else if (event.start.epochSecond < range.first && event.end.epochSecond > range.last) {
+                // This is a multi-day event that starts before today and ends after today
+                events.add(event)
+            }
+        }
+        multi[it] = events.sortedBy(Event::start)
+    }
     return multi.toSortedMap()
 }
