ad43c3e20f60bc69d19e86ce771f830132d1f0c2
NovaFox161 <novamaday@gmail.com>
2023-08-27 20:02:36 -0500

    Remove unused connection factory bean

--- cam/src/main/kotlin/org/dreamexposure/discal/cam/config/WebFluxConfig.kt
+++ cam/src/main/kotlin/org/dreamexposure/discal/cam/config/WebFluxConfig.kt
@@ -1,11 +1,6 @@
 package org.dreamexposure.discal.cam.config
 
-import io.r2dbc.spi.ConnectionFactories
-import io.r2dbc.spi.ConnectionFactory
-import io.r2dbc.spi.ConnectionFactoryOptions
-import org.dreamexposure.discal.core.config.Config
 import org.dreamexposure.discal.core.utils.GlobalVal
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.http.codec.json.KotlinSerializationJsonDecoder
@@ -16,19 +11,6 @@ import org.springframework.web.reactive.config.WebFluxConfigurer
 @Configuration
 @EnableWebFlux
 class WebFluxConfig: WebFluxConfigurer {
-
-    @Bean(name = ["mysqlDatasource"])
-    fun mysqlConnectionFactory(): ConnectionFactory {
-        return ConnectionFactories.get(ConnectionFactoryOptions.builder()
-            .option(ConnectionFactoryOptions.DRIVER, "pool")
-            .option(ConnectionFactoryOptions.PROTOCOL, "mysql")
-            .option(ConnectionFactoryOptions.HOST, Config.SQL_HOST.getString())
-            .option(ConnectionFactoryOptions.PORT, Config.SQL_PORT.getInt())
-            .option(ConnectionFactoryOptions.USER, Config.SQL_USER.getString())
-            .option(ConnectionFactoryOptions.PASSWORD, Config.SQL_PASS.getString())
-            .option(ConnectionFactoryOptions.DATABASE, Config.SQL_DB.getString())
-                .build())
-    }
 
     override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
         val codecs = configurer.defaultCodecs()
--- server/src/main/kotlin/org/dreamexposure/discal/server/config/WebFluxConfig.kt
+++ server/src/main/kotlin/org/dreamexposure/discal/server/config/WebFluxConfig.kt
@@ -1,14 +1,9 @@
 package org.dreamexposure.discal.server.config
 
-import io.r2dbc.spi.ConnectionFactories
-import io.r2dbc.spi.ConnectionFactory
-import io.r2dbc.spi.ConnectionFactoryOptions
-import org.dreamexposure.discal.core.config.Config
 import org.dreamexposure.discal.core.utils.GlobalVal
 import org.springframework.boot.web.server.ConfigurableWebServerFactory
 import org.springframework.boot.web.server.ErrorPage
 import org.springframework.boot.web.server.WebServerFactoryCustomizer
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.http.codec.ServerCodecConfigurer
@@ -32,19 +27,6 @@ class WebFluxConfig : WebServerFactoryCustomizer<ConfigurableWebServerFactory>, 
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .maxAge(600)
-    }
-
-    @Bean(name = ["mysqlDatasource"])
-    fun mysqlConnectionFactory(): ConnectionFactory {
-        return ConnectionFactories.get(ConnectionFactoryOptions.builder()
-            .option(ConnectionFactoryOptions.DRIVER, "pool")
-            .option(ConnectionFactoryOptions.PROTOCOL, "mysql")
-            .option(ConnectionFactoryOptions.HOST, Config.SQL_HOST.getString())
-            .option(ConnectionFactoryOptions.PORT, Config.SQL_PORT.getInt())
-            .option(ConnectionFactoryOptions.USER, Config.SQL_USER.getString())
-            .option(ConnectionFactoryOptions.PASSWORD, Config.SQL_PASS.getString())
-            .option(ConnectionFactoryOptions.DATABASE, Config.SQL_DB.getString())
-                .build())
     }
 
     override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
