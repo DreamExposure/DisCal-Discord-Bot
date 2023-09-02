34c0d6ccbb2fb622f445c6fa170a142cd47a1205
NovaFox161 <novamaday@gmail.com>
2023-09-01 21:38:34 -0500

    Fix serialization

--- core/src/main/kotlin/org/dreamexposure/discal/core/serializers/SnowflakeMapper.kt
+++ core/src/main/kotlin/org/dreamexposure/discal/core/serializers/SnowflakeMapper.kt
@@ -3,7 +3,6 @@ package org.dreamexposure.discal.core.serializers
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.fasterxml.jackson.databind.module.SimpleModule
@@ -18,15 +17,13 @@ class SnowflakeMapper: SimpleModule() {
 
     class SnowflakeSerializer : StdSerializer<Snowflake>(Snowflake::class.java) {
         override fun serialize(value: Snowflake?, gen: JsonGenerator?, provider: SerializerProvider?) {
-            gen?.writeStartObject()
-            gen?.writeStringField("Snowflake", value?.asString())
-            gen?.writeEndObject()
+            gen?.writeString(value?.asString())
         }
     }
 
     class SnowflakeDeserializer: StdDeserializer<Snowflake>(Snowflake::class.java) {
         override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): Snowflake {
-            val raw = p?.codec?.readTree<JsonNode>(p)?.get("Snowflake")?.asText()
+            val raw = p?.valueAsString
             return if (raw != null) Snowflake.of(raw) else throw IllegalStateException()
         }
     }
