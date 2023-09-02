22fa295f487459d46d36503936090db6205f1abe
NovaFox161 <novamaday@gmail.com>
2023-09-01 21:38:34 -0500

    Fix serialization

--- core/src/main/kotlin/org/dreamexposure/discal/core/serializers/DurationMapper.kt
+++ core/src/main/kotlin/org/dreamexposure/discal/core/serializers/DurationMapper.kt
@@ -3,7 +3,6 @@ package org.dreamexposure.discal.core.serializers
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.fasterxml.jackson.databind.module.SimpleModule
@@ -18,15 +17,13 @@ class DurationMapper: SimpleModule() {
 
     class DurationSerializer : StdSerializer<Duration>(Duration::class.java) {
         override fun serialize(value: Duration?, gen: JsonGenerator?, provider: SerializerProvider?) {
-            gen?.writeStartObject()
-            gen?.writeStringField("Duration", value?.toMillis().toString())
-            gen?.writeEndObject()
+            gen?.writeString(value?.toMillis().toString())
         }
     }
 
     class DurationDeserializer: StdDeserializer<Duration>(Duration::class.java) {
         override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): Duration {
-            val raw = p?.codec?.readTree<JsonNode>(p)?.get("Duration")?.asText()
+            val raw = p?.valueAsString
             return if (raw != null) Duration.ofMillis(raw.toLong()) else throw IllegalStateException()
         }
     }
