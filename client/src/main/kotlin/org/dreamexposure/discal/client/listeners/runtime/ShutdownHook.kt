74ee20b5865e19a9ac2af222226a23086ef066e3
NovaFox161 <novamaday@gmail.com>
2024-03-20 19:43:31 -0500

    Move shutdown hook package location

--- client/src/main/kotlin/org/dreamexposure/discal/client/listeners/runtime/ShutdownHook.kt
+++ client/src/main/kotlin/org/dreamexposure/discal/client/listeners/runtime/ShutdownHook.kt
@@ -1,9 +1,9 @@
-package org.dreamexposure.discal.client.service
+package org.dreamexposure.discal.client.listeners.runtime
 
 import discord4j.core.GatewayDiscordClient
 import jakarta.annotation.PreDestroy
 import org.dreamexposure.discal.core.logger.LOGGER
-import org.dreamexposure.discal.core.utils.GlobalVal.STATUS
+import org.dreamexposure.discal.core.utils.GlobalVal
 import org.springframework.stereotype.Component
 
 // Required to be a component for shutdown hook due to lifecycle management of the discord client
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component
 class ShutdownHook(private val discordClient: GatewayDiscordClient) {
     @PreDestroy
     fun onShutdown() {
-        LOGGER.info(STATUS, "Shutting down shard")
+        LOGGER.info(GlobalVal.STATUS, "Shutting down shard")
 
         discordClient.logout().subscribe()
     }
