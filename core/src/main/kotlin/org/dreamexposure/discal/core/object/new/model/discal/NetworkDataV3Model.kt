e5ba9b74f33af95b9dd834368d20d9f02594273f
NovaFox161 <nova@maday.dev>
2025-07-27 21:34:38 -0500

    Just trying to see if this does what I want, doubt it tho

--- core/src/main/kotlin/org/dreamexposure/discal/core/object/new/model/discal/NetworkDataV3Model.kt
+++ core/src/main/kotlin/org/dreamexposure/discal/core/object/new/model/discal/NetworkDataV3Model.kt
@@ -10,12 +10,15 @@ data class NetworkDataV3Model(
     val camStatus: List<InstanceDataV3Model> = emptyList(),
     val websiteStatus: InstanceDataV3Model? = null,
     val botStatus: List<BotInstanceDataV3Model> = emptyList(),
-
-    val expectedShardCount: Int = botStatus.getOrNull(0)?.shardCount ?: 0,
-    val currentShardCount: Int = botStatus.size,
-    val totalGuildsCount: Int = botStatus.sumOf(BotInstanceDataV3Model::guilds),
-    val totalMemoryInGb: Double =
-        apiStatus.map(InstanceDataV3Model::memory)
+) {
+    val expectedShardCount: Int
+        get() = botStatus.getOrNull(0)?.shardCount ?: 0
+    val currentShardCount: Int
+        get() = botStatus.size
+    val totalGuildsCount: Int
+        get() = botStatus.sumOf(BotInstanceDataV3Model::guilds)
+    val totalMemoryInGb: Double
+        get() = apiStatus.map(InstanceDataV3Model::memory)
             .plus(camStatus.map(InstanceDataV3Model::memory))
             .plus(websiteStatus?.memory ?: 0.0)
             .plus(botStatus.map { it.instanceData.memory })
@@ -24,5 +27,5 @@ data class NetworkDataV3Model(
             .times(100)
             .roundToInt()
             .div(100)
-            .toDouble(),
-)
+            .toDouble()
+}
