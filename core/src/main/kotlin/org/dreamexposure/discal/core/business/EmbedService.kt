d4abfcf93f6cd7f76b03fa7c969dcf78b55984dd
NovaFox161 <nova@maday.dev>
2026-02-13 02:23:05 -0600

    Lmao, made the changes to the wrong method, now it should actually work properly I think

--- core/src/main/kotlin/org/dreamexposure/discal/core/business/EmbedService.kt
+++ core/src/main/kotlin/org/dreamexposure/discal/core/business/EmbedService.kt
@@ -139,16 +139,11 @@ class EmbedService(
         }
 
         // Show events
-        val today = Instant.now().atZone(calendar.timezone).truncatedTo(ChronoUnit.DAYS)
-
-        for (i in 0..6) {
-            val dateToDisplay = today.plusDays(i.toLong())
-            val events = truncatedEvents[dateToDisplay].orEmpty()
-
-            val title = dateToDisplay.toInstant().humanReadableDate(calendar.timezone, settings.interfaceStyle.timeFormat, longDay = true)
+        truncatedEvents.forEach { date ->
+            val title = date.key.toInstant().humanReadableDate(calendar.timezone, settings.interfaceStyle.timeFormat, longDay = true)
 
             // sort events
-            val sortedEvents = events.sortedBy { it.start }
+            val sortedEvents = date.value.sortedBy { it.start }
 
             val content = StringBuilder()
 
@@ -166,13 +161,13 @@ class EmbedService(
                         .append("| ")
                 } else {
                     // Add start text
-                    var str = if (it.start.isBefore(dateToDisplay.toInstant())) {
+                    var str = if (it.start.isBefore(date.key.toInstant())) {
                         "${getCommonMsg("generic.time.continued", settings.locale)} - "
                     } else {
                         "${it.start.humanReadableTime(it.timezone, settings.interfaceStyle.timeFormat)} - "
                     }
                     // Add end text
-                    str += if (it.end.isAfter(dateToDisplay.toInstant().plus(1, ChronoUnit.DAYS))) {
+                    str += if (it.end.isAfter(date.key.toInstant().plus(1, ChronoUnit.DAYS))) {
                         getCommonMsg("generic.time.continued", settings.locale)
                     } else {
                         "${it.end.humanReadableTime(it.timezone, settings.interfaceStyle.timeFormat)} "
@@ -233,11 +228,16 @@ class EmbedService(
         }
 
         // Show events
-        groupedEvents.forEach { date ->
-            val title = date.key.toInstant().humanReadableDate(calendar.timezone, settings.interfaceStyle.timeFormat, longDay = true)
+        val today = Instant.now().atZone(calendar.timezone).truncatedTo(ChronoUnit.DAYS)
+
+        for (i in 0..6) {
+            val dateToDisplay = today.plusDays(i.toLong())
+            val events = groupedEvents[dateToDisplay].orEmpty()
+
+            val title = dateToDisplay.toInstant().humanReadableDate(calendar.timezone, settings.interfaceStyle.timeFormat, longDay = true)
 
             // sort events
-            val sortedEvents = date.value.sortedBy { it.start }
+            val sortedEvents = events.sortedBy { it.start }
 
             val content = StringBuilder()
 
@@ -255,13 +255,13 @@ class EmbedService(
                         .append("| ")
                 } else {
                     // Add start text
-                    var str = if (it.start.isBefore(date.key.toInstant())) {
+                    var str = if (it.start.isBefore(dateToDisplay.toInstant())) {
                         "${getCommonMsg("generic.time.continued", settings.locale)} - "
                     } else {
                         "${it.start.humanReadableTime(it.timezone, settings.interfaceStyle.timeFormat)} - "
                     }
                     // Add end text
-                    str += if (it.end.isAfter(date.key.toInstant().plus(1, ChronoUnit.DAYS))) {
+                    str += if (it.end.isAfter(dateToDisplay.toInstant().plus(1, ChronoUnit.DAYS))) {
                         getCommonMsg("generic.time.continued", settings.locale)
                     } else {
                         "${it.end.humanReadableTime(it.timezone, settings.interfaceStyle.timeFormat)} "
@@ -280,12 +280,6 @@ class EmbedService(
                 // Finish event
                 content.append("```\n")
             }
-            if (content.isBlank()) {
-                content.append("```\n")
-                    .append("[ No Scheduled Events ]")
-                    .append("\n```")
-            }
-
             calculatedEmbedCharacterLength += title.length + content.toString().embedFieldSafe().length
 
             // max embed length is 6000 characters, we are going to go a bit under that in just for extra safety
